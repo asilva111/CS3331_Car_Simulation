01ec23fa9137bcd6adbba9c1c63cbcbd1d2f293e
asilva111 <45497798+asilva111@users.noreply.github.com>
2019-09-08 23:16:09 -0600

    Add files via upload
    >Added header and more comments

--- Car.java
+++ Car.java
@@ -1,3 +1,17 @@
+/*CS3331 - Advanced Object-oriented programming - Dr.Roach - M,W 3:00pm - 4:20pm
+ * This program simulates a 3 mile circuit where n number of cars can participate. Each mile has a specified
+ * top speed (20mph, 60mph, and 30mph), and a position (distance) based on arbitrary 'time'. 
+ * 
+ * The timer will stop running only after all cars have finished the track.
+ * The cars will stop running once they finish the track.
+ * 
+ * Cars have to start running 1 minute apart from each other and they have the same acceleration.
+ * 
+ * It is important that cars do not go over the speed limit before entering a new section, which is why a slow
+ * down action is required.
+ * 
+ * Last updated: Sep 8th, 2019 by Andres Silva - 80588336
+ **************************************************************************************************************/
 import java.lang.Math;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -17,7 +31,6 @@ public class Car {
 	
 	private int currentSegment;
 	
-
 	private static double deltaTime = .01; //Specified change in time.
 
 	public Car(String newName, double speed, double Acceleration) {
@@ -32,8 +45,7 @@ public class Car {
 		running = false;
 		finished = false;
 		
-		currentSegment = 1;
-			
+		currentSegment = 1;	
 	}
 	
 	public static double round(double value, int places) { // Rounding method taken from: https://stackoverflow.com/questions/2808535/round-a-double-to-2-decimal-places
@@ -55,7 +67,6 @@ public class Car {
 	private void setVelocity() { 
 		if((velocity + (acceleration * deltaTime) <= segmentSpeed) && velocity + (acceleration * deltaTime) >= 0) {
 			velocity += acceleration * deltaTime; //Update velocity only when it has not reached the top speed and is greater than 0.
-
 		}
 		else { //Else cap speed at indicated value.
 			velocity = segmentSpeed;
@@ -65,7 +76,6 @@ public class Car {
 	private void setDistance() { 
 		distance += velocity * deltaTime; 
 		//Formula: distance = velocity * deltaTime
-
 	}
 	
 	private static double getStoppingDistance(Car R, double FinalVelocity) { //Calculate distance to begin slow down.
@@ -77,7 +87,7 @@ public class Car {
 		int doneCars = 0;
 		
 		for(int i = 0; i < Cars.length; i++) { 
-			if (Cars[i].distance >= trackLength) { //If a runner traveled the track length, count it as done.
+			if (Cars[i].finished == true) { //If a runner traveled the track length, count it as done.
 				doneCars++;
 			}
 		}
@@ -89,7 +99,6 @@ public class Car {
 		else { //Else, return true
 			return true;
 		}
-		
 	}
 	
 	private static void printLine(Car[] Cars, double time) { //Print distance of all runners at specified time.
@@ -115,40 +124,41 @@ public class Car {
 		double segment3TopSpeed = (30 * 1.467);
 		
 		double segment1Length = 5280; //All segments are 1 mile (5280 ft) in length.
-		double segment2Length = segment1Length + 5280; 
+		double segment2Length = segment1Length + 5280; //In feet, where does the segment ends.
 		double segment3Length = segment2Length + 5280; 
 	
 		/**Car declaration********************************************/
-		
 		Car Cars[] = new Car[3];
 		
-		Cars[0] = new Car("A", segment1TopSpeed , 15); // A, 20 miles in feet per second, 15 feet per sec^2 
+		Cars[0] = new Car("A", segment1TopSpeed , 15); // A, 20 miles in feet per second, 15 feet per sec^2.
 		Cars[1] = new Car("B", segment1TopSpeed , 15);
 		Cars[2] = new Car("C", segment1TopSpeed , 15);
 		/************************************************************/
 		
 		/**Formating*************************************************/
-		
 		for(int i = 0; i < Cars.length; i++) { //For all runners print name.
 			System.out.print("\tCar " + Cars[i].name + "\t\t");
 		}
 		
 		System.out.print("\nTime");
+		
 		for(int i = 0; i < Cars.length ;i++) {
 			System.out.print("\tSpeed   Location");
 		}
+		/************************************************************/
 		
 		while(trackStatus(Cars, round(time,2), trackLength) == false) { //While there is still a car that has not finished driving,
 
 			for(int i = 0; i < Cars.length; i++) { 
 
-				if(round(time,2) * 60 == i * 60 * 60) { //Mark cars as running one minute apart from each other.
+				if(round(time,2) * 60 == i * 60 * 60) { //Mark cars as running one minute apart from each other, starting with the first car.
 					Cars[i].running = true;	
 				}
-				if(Cars[i].distance > 0 && Cars[i].distance < segment1Length) {
+				
+				if(Cars[i].distance > 0 && Cars[i].distance < segment1Length) { //If a car is within a section of the circuit, mark it as such.
 					Cars[i].currentSegment = 1;
 				}
-				if(Cars[i].distance > segment1Length && Cars[i].distance < segment2Length) {
+				if(Cars[i].distance > segment1Length && Cars[i].distance < segment2Length) { 
 					Cars[i].currentSegment = 2;
 				}
 				if(Cars[i].distance > segment2Length && Cars[i].distance < segment3Length) {
@@ -163,55 +173,43 @@ public class Car {
 						Cars[i].segmentSpeed = segment2TopSpeed; //Change segment speed.	
 				}
 				
-				//If car is running and about to enter the second segment, slow down.
+				//If car is running and about to enter the third segment, slow down.
 				stoppingValue = getStoppingDistance(Cars[i], segment3TopSpeed); 				
 				if( Cars[i].running == true &&
 					Cars[i].slowingDown == false && 
 					Cars[i].currentSegment == 2 &&
-					Cars[i].distance >= (segment2Length - stoppingValue) - .5 && 
+					Cars[i].distance >= (segment2Length - stoppingValue) - .5 &&  //Check if car is within range of where it should stop decelerating.
 					Cars[i].distance <= (segment2Length - stoppingValue) + .5){ 
 					
-						Cars[i].setAcceleration(Cars[i].getAcceleration() * -1);
+						Cars[i].setAcceleration(Cars[i].getAcceleration() * -1); //Set acceleration to negative to slow down.
 						Cars[i].slowingDown = true;
-
 				}
+				
 				//If desired velocity reached, change acceleration back to positive and keep going.
 				if( Cars[i].running == true &&
-					Cars[i].velocity <= segment3TopSpeed -.5 &&	
+					Cars[i].velocity < segment3TopSpeed -.5 &&	 //The .5 makes sure the velocity is not very close to the limit.
 					Cars[i].distance > segment2Length ) {
 						
 						Cars[i].currentSegment = 3;
-						Cars[i].segmentSpeed = segment3TopSpeed;
-						Cars[i].setAcceleration(Cars[i].getAcceleration() * -1);
-						
+						Cars[i].segmentSpeed = segment3TopSpeed; //Set new segment top speed.
+						Cars[i].setAcceleration(Cars[i].getAcceleration() * -1); //Set acceleration to positive.
 				}
 								
-				if(Cars[i].running == true) {
-					Cars[i].setVelocity(); //Update car location and velocity
+				if(Cars[i].running == true && Cars[i].finished == false) { //Only update when car is running and not finished.
+					Cars[i].setVelocity(); //Update velocity and distance.
 					Cars[i].setDistance();
 				}
-
 			}
-			
-
-			
+		
 			if(round(time,2) % printInterval == 0) { //Every time interval, print data.
 				printLine(Cars, round(time,2));
 			}
 			
-			time += deltaTime; //Increase time
-		}
-		
-		
+			time += deltaTime; //Increase time.
+			
+		}//Close while loop.
 
-	
-	
-	
-	
-	
 	}//Close main
-	
-	
 }//Close class.
 	
 	
